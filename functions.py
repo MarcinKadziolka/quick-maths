e801a740657575c690a238d41c6bcc89dcbacd42
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2023-07-04 23:48:33 +0200

    Allow to choose center or top left for coordinates of rects

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def draw_text(text, font, text_color, x, y, screen): 
+def draw_text(text, font, text_color, x, y, screen, center=True): 
     text_obj = font.render(text, font, text_color)
-    text_rect = text_obj.get_rect(center=(x, y))
+    text_rect = text_obj.get_rect(topleft=(x, y))
+    if center:
+        text_rect.center = x, y
     screen.blit(text_obj, text_rect)
