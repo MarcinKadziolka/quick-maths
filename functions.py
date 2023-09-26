3a0ce0f8ffa42a8ad4e5870cdc4242529bf82b1d
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2023-09-26 11:22:10 +0200

    Add function to retrieve category_id from database

--- functions.py
+++ functions.py
@@ -7,3 +7,12 @@ def draw_text(text, x, y, screen, center=True, text_color=settings.colors.BLACK,
     if center:
         text_rect.center = x, y
     screen.blit(text_obj, text_rect)
+
+
+def select_category_id(cursor, operation, digit, amount):
+    category_name = f"{operation}_digits_{digit}_amount_{amount}"
+    cursor.execute(
+        "SELECT category_id FROM category WHERE name=?",
+        (category_name,),
+    )
+    return cursor.fetchone()[0]
