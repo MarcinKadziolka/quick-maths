82ac7ecfbbad8c95219713378af39cdf974cc27c
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-03-05 23:35:37 +0100

    Add ScreenSize class; Colors: tuples -> lists

--- settings.py
+++ settings.py
@@ -1,26 +1,53 @@
+from dataclasses import dataclass
 import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 1360
-SCREEN_HEIGHT = 768
-MID_WIDTH = SCREEN_WIDTH / 2
-MID_HEIGHT = SCREEN_HEIGHT / 2
-SCREEN_THIRDS = [SCREEN_WIDTH/6, SCREEN_WIDTH/2, SCREEN_WIDTH*5/6]
+
+@dataclass
+class ScreenSize:
+    x: int
+    y: int
+
+    @property
+    def mid_x(self) -> float:
+        return self.x / 2
+
+    @property
+    def mid_y(self) -> float:
+        return self.y / 2
+
+    @property
+    def left_third(self) -> float:
+        return self.x / 6
+
+    @property
+    def middle_third(self) -> float:
+        return self.x / 2
+
+    @property
+    def right_third(self) -> float:
+        return self.x * 5 / 6
+
+
+SCREEN_SIZE = ScreenSize(1366, 768)
+
 DISTANCE = 80
 SLEEP_DURATION = 0.1
 
 main_font = pygame.font.SysFont("rasa", 80)
 main_font_small = pygame.font.SysFont("rasa", 50)
 equation_font_small = pygame.font.SysFont("rasa", 70)
-# COLORS
+
+
 class COLORS:
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    BACKGROUND = (167, 199, 250)
-    GREEN = (127, 255, 212)
-    LIGHT_GREEN = (179, 255, 230)
-    LIGHT_RED = (180, 190, 250)
-    GRAY = (220,220,220)
+    WHITE = [255, 255, 255]
+    BLACK = [0, 0, 0]
+    BACKGROUND = [167, 199, 250]
+    GREEN = [127, 255, 212]
+    LIGHT_GREEN = [179, 255, 230]
+    LIGHT_RED = [180, 190, 250]
+    GRAY = [220, 220, 220]
+
 
 colors = COLORS()
